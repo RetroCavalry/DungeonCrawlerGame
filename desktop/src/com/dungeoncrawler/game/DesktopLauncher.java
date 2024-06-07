1f0cb3972590f4874ab3f0921312be20461f4a14
RetroCavalry <163904407+RetroCavalry@users.noreply.github.com>
2024-06-08 01:47:17 +0300

    0.0.1

---
+++ desktop/src/com/dungeoncrawler/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.dungeoncrawler.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.dungeoncrawler.game.DungeonCrawlerGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("DungeonCrawlerGame");
+		new Lwjgl3Application(new DungeonCrawlerGame(), config);
+	}
+}
